33fe1fd55f76647c1901ea573172e397abd839d6
priscila monteiro <pririmonteiro@me.com>
2021-12-08 12:00:20 -0800

    function find completed

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,9 @@
+const db = require('../../data/db-config')
+const Scheme = require('./scheme-model')
+
+
+
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,8 +12,21 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
 
+
+async function checkSchemeId (req, res, next) {
+  try{
+    const { scheme_id } = req.params
+    const scheme = await Scheme.findById(scheme_id)
+    if(scheme){
+      req.scheme = scheme
+      next()
+    } else {
+      next({ status:404, message: `scheme with scheme_id ${scheme_id} not found`})
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 /*
