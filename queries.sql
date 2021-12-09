c3aa7ee6613f705d82052b1252e3f2764500e763
priscila monteiro <pririmonteiro@me.com>
2021-12-08 19:22:24 -0800

    strech 2 down 4 to go but I am tired

--- queries.sql
+++ queries.sql
@@ -33,3 +33,48 @@ order by productname
 ;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+select
+id as Order_Id, CompanyName as customer_Company_Name,LastName as employee_Lastname
+from [Order]
+JOIN employee as E
+on [order].employeeid=E.Id
+JOIN customer as C
+on [order].CustomerId=C.Id
+;
+
+
+-- Find the number of shipments by each shipper. Displays 5654 for Federal, 5572 for Speedy and 5592 for united
+
+select
+count([order].id) as shipments_count, CompanyName
+from [Order]
+JOIN shipper as S
+on [order].ShipVia=S.Id
+group by companyName
+;
+
+
+-- Find the top 5 best performing employees measured in number of orders. 1964	Janet Leverling, 1918	Nancy Davolio, 1907	Margaret Peacock, 1859	Steven Buchanan, 1849	Michael Suyama
+
+select 
+count([order].id) as number_orders, firstname || ' ' || lastname as employee
+from [Order]
+JOIN employee as E
+on [order].employeeid=E.Id
+group by employee 
+order by number_orders desc
+Limit 5
+;
+
+
+-- Find the top 5 best performing employees measured in revenue.
+
+
+-- Find the category that brings in the least revenue.
+
+
+-- Find the customer country with the most orders.
+
+
+-- Find the shipper that moves the most cheese measured in units.
