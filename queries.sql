b92c32511f0a0367adf1042d3aba458dfe867a88
priscila monteiro <pririmonteiro@me.com>
2021-12-08 16:24:50 -0800

    strech- so far 2 queries done

--- queries.sql
+++ queries.sql
@@ -1,8 +1,25 @@
+
+
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT 
+ ProductName, CategoryName
+FROM Product AS P
+LEFT JOIN Category AS C
+ON P.CategoryID = C.ID;
+
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+select
+id, CompanyName
+from [Order]
+JOIN shipper as S
+on [order].ShipVia=S.Id
+where [order].orderdate < '2012-08-09'
+;
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
